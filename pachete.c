10c013796d69b7ea7245e6e1aaca24b64984bc2b
RagnarokMew <101945097+RagnarokMew@users.noreply.github.com>
2024-12-12 17:34:16 +0200

    Implement task 2.6 + 2.7 skeleton and various fixes incl package distribution
    Signed-off-by: RagnarokMew <101945097+RagnarokMew@users.noreply.github.com>

--- pachete.c
+++ pachete.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<math.h>
 
 typedef struct {
   int id;
@@ -27,7 +28,7 @@ typedef struct {
 
 void readInput(int* nrC, int* nrP, cartier** cartiere, pachet** pachete); // Cerinta 1.1
 
-void freeAll(int nrC, int nrP, cartier* cartiere, pachet* pachete, postas* postasi); // Utility func
+void freeAll(int nrC, int nrP, cartier* cartiere, pachet* pachete, postas* postasi); // Utility function, elibereaza memoria alocata dinamic
 
 void outputCerinta1(int nrC, cartier* cartiere, int nrP, pachet* pachete); // Cerinta 1
 
@@ -39,9 +40,19 @@ int condSortPackage(pachet a, pachet b); // Cerinta 1.4
 
 void sortPackages(pachet* pachete, int nrP, int (*cmp)(pachet,pachet)); // Cerinta 1.4
 
-void removeReverse(char* string); // Cerinta 1.5
+void removeReverse(char* string); // Cerinta 1.5 #!NOTE: tested should work remove when done
 
 void calculateCode(pachet* p); // Cerinta 1.5
+
+// #!WARN: nush cum ar trb sa functioneze asta, modifica doar pachetele sale, toate pachetele etc
+void postasDoBad(postas* p); // Cerinta 2.6
+
+void alterCodes(int* code, int idPostas); // Cerinta 2.6
+
+int isPrime(int n); // Helper function, verf daca nr este prim 
+
+//#!WARN: nush cum ar trb sa implementez asta tbh, like cred ca ar trb sa trimit si toate
+double calculateScore(postas p); // Cerinta 2.7
 
 int main()
 {
@@ -54,6 +65,12 @@ int main()
   cartier* cartiere;
   postas* postasi;
   int nrP, nrC;
+
+  char test[100]="Ana, are cel mult 3 mere";
+  removeReverse(test);
+  printf("%s",test);
+  return 0;
+
   readInput(&nrC, &nrP, &cartiere, &pachete);
 
   distributePackage(nrC, &postasi, nrP, pachete);//!WARN: Nush daca asta trb aici dar pun sa nu plang dupa;
@@ -113,8 +130,8 @@ void readInput(int* nrC, int* nrP, cartier** cartiere, pachet** pachete) {
       scanf("%d", &(*pachete)[i].adresa[j]);
     scanf("%d", &(*pachete)[i].prioritate);
     scanf("%f", &(*pachete)[i].greutate);
-    char buff[1024];
-    fgets(buff, 1024, stdin);
+    char buff[101];
+    fgets(buff, 100, stdin);
     buff[strlen(buff) - 1]= '\0'; //#!WARN: This might actually not be good because msg might need to have endl
     (*pachete)[i].mesaj = (char*)malloc(strlen(buff) + 1);
     strcpy((*pachete)[i].mesaj, buff);
@@ -123,6 +140,7 @@ void readInput(int* nrC, int* nrP, cartier** cartiere, pachet** pachete) {
     (*pachete)[i].strada = 0;
     (*pachete)[i].idCartier = 0;
     (*pachete)[i].numar = 0;
+    (*pachete)[i].codificareMesaj = 0;
     processAddress(&(*pachete)[i]);
   }
 }
@@ -179,13 +197,13 @@ void distributePackage(int nrC, postas** postasi, int nrP, pachet* pachete) {
   // Initializeaza postasi
   *postasi = (postas*)malloc(nrC * sizeof(postas));
   for(int i = 0; i < nrC; i++) {
-    (*postasi)[i].id = 0;
+    (*postasi)[i].id = i;
     (*postasi)[i].nrPachete = 0;
   }
   
   // Distribuieste pachete
   for(int i = 0; i < nrP; i++) {
-    (*postasi)[pachete[i].strada].distribuite[(*postasi)[pachete[i].strada].nrPachete] = pachete[i].id;
+    (*postasi)[pachete[i].idCartier].distribuite[(*postasi)[pachete[i].idCartier].nrPachete] = pachete[i].id;
     (*postasi)[pachete[i].strada].nrPachete++;
   }
 }
@@ -222,11 +240,63 @@ void removeReverse(char* string) {
   while(token != NULL)
   {
     strcpy(cpy + strlen(token), cpy);
-    strcpy(cpy, token);
+    strncpy(cpy, token, strlen(token));
     token = strtok(NULL, punctuation);
   }
+  strcpy(string,cpy);
+  free(cpy);
 } 
 
 void calculateCode(pachet* p) {
+  for(int i = 0; i < strlen(p->mesaj); i++)
+    p->codificareMesaj += i + p->mesaj[i];
 
-} 
+  p->codificareMesaj %= (p->numar + p->strada + 1);
+}
+
+// #!WARN: nush cum ar trb sa functioneze asta, modifica doar pachetele sale, toate pachetele etc
+void postasDoBad(postas* p); //Cerinta 2.6
+
+void alterCodes(int* code, int idPostas) {
+  int prime_factors[11];
+  int num = 0;
+
+  //edgecases, apoi skip la alterarea codurilor
+  if(idPostas == 0 || idPostas == 1)
+  {
+    prime_factors[0] = idPostas;
+    num = 1;
+    goto startalter;
+  }
+  
+  //gasirea factorilor
+  for(int i = 2; i <= sqrt(idPostas) && i < 32; i++)
+    if(idPostas % i == 0 && isPrime(i))
+      prime_factors[num++] = i;
+
+  startalter:
+    for(int i = 0; i < num; i++)
+      *code ^= (1<<prime_factors[i]); // negarea bitului de pe poz; 1^1 = 0; 0^1 = 1; 1^0 = 1; 0^0 = 0;
+  return;
+}
+
+int isPrime(int n) {
+  if(n == 0 || n == 1 || (n != 2 && (n & 1) == 1) ) //cond pt care un nr nu e prim + verf imp cu bitwise 1 bc I can 
+    return 0;
+
+  for(int i = 2; i <= sqrt(n); i++)
+    if(n % i == 0)
+      return 0;
+  return 1;
+}
+
+//#!TODO implement the function
+double calculateScore(postas p) {
+  if(p.nrPachete == 0)
+    return 0;
+  
+  int correct_dist = 0;
+
+  //#!NOTE: Implementation goes here
+  return 1.* correct_dist / p.nrPachete;
+}
